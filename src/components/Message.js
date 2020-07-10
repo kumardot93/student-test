1a467df69b9a17443dfb8ed751d4646495973501
aditya <kumar.93@iitj.ac.in>
2020-07-10 09:38:15 +0530

    adjusted message to show for ever

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Message(props) {
-	setTimeout(() => {
-		let el = document.getElementById('message');
-		ReactDOM.unmountComponentAtNode(el);
-		el.style.display = 'none';
-	}, 10000);
+	// setTimeout(() => {
+	// 	let el = document.getElementById('message');
+	// 	ReactDOM.unmountComponentAtNode(el);
+	// 	el.style.display = 'none';
+	// }, 10000);
 	return <React.Fragment>{props.message}</React.Fragment>;
 }
 
